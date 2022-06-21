5f6cdf1bc941e6e8ef004bfa321de166bc603e2c
viet.tran <viettran295@gmail.com>
2022-06-21 20:26:25 +0200

    init Bot in C#

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with EchoBot .NET Template version v4.15.2
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace EchoBot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (string.Equals(turnContext.Activity.Text, "wait", System.StringComparison.InvariantCultureIgnoreCase))
+            {
+                await turnContext.SendActivitiesAsync(
+                    new Activity[] {
+                        new Activity { Type = ActivityTypes.Typing },
+                        new Activity { Type = "delay", Value= 3000 },
+                        MessageFactory.Text("Finished typing", "Finished typing"),
+                    },
+                    cancellationToken);
+            }
+            else
+            {
+                var replyText = $"Echo: {turnContext.Activity.Text}. Say 'wait' to watch me type.";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
